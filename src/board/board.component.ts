99a2f01b3e7658dc6d7265d9ebcbf07ad64fdd94
Christine Miaekivi <christine.miaekivi@gmail.com>
2025-05-23 15:39:55 +0200

    passed button state and id to board component

--- src/board/board.component.ts
+++ src/board/board.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 
+type button = {
+  id: number,
+  isClicked: boolean
+}
+
 @Component({
   selector: 'app-board',
-  imports: [ButtonComponent],
+  imports: [ ButtonComponent ],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
 
+  //isEvenRound = false
+  
+  //control what to display on button
+  ButtonDisplay: 'X'|'O'|'' = ''
+
+   //get info from child
+    getButtonInfo (info: any) {
+    console.log(`im button num ${info.id}`)
+    console.log(`am i clicked: ${info.isClicked}`)
+  }
 }
--- src/button/button.component.ts
+++ src/button/button.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+type button = {
+  id: number | any,
+  isClicked: boolean
+}
 
 @Component({
   selector: 'app-button',
@@ -9,5 +13,30 @@ import { input } from '@angular/core';
 })
 
 export class ButtonComponent {
-  ID = input<number>()
+  @Input() ID: number = 0
+  @Input() OnDisplay: 'X'|'O'|'' = '';
+
+  IsButtonClicked = false
+
+  // board = [
+  //   [1, null, 0],
+  //   [null, null,1 ],
+  //   [1, null, 0],
+  // ]
+
+  //let parent component know that a button was clicked
+  //than parent will decide what to put on display
+  @Output() ButtonEmitter = new EventEmitter<button>();
+
+  sendButtonStateUp(){
+    this.ButtonEmitter.emit({ 
+      id: this.ID, 
+      isClicked: this.IsButtonClicked 
+    })
+  }
+
+  handleClick(){
+    this.IsButtonClicked = true
+    this.sendButtonStateUp()
+  }
 }
